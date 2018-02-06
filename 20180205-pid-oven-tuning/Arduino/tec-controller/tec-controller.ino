7e0b3d532713611bc370d6f7ca34b2d3f40b20b9
Jason Pepas <jasonpepas@gmail.com>
2018-02-05 22:36:20 -0600

    run 24

--- 20180205-pid-oven-tuning/Arduino/tec-controller/tec-controller.ino
+++ 20180205-pid-oven-tuning/Arduino/tec-controller/tec-controller.ino
@@ -22,7 +22,7 @@ double cool_kd = 0.5; // warning: these are out of date
 // reasonable values for 5V with limit of 205:
 double heat_kp = 16.0;
 double heat_ki = 2.0;
-double heat_kd = 3.2;
+double heat_kd = 6.4;
 
 //double upper_limit = 65.0; // approximately 1000mA at 12V.
 double upper_limit = 205.0; // approximately 1000mA at 5V.
--- 20180205-pid-oven-tuning/README.md
+++ 20180205-pid-oven-tuning/README.md
@@ -255,3 +255,14 @@ revisiting the tuning on my TEC chamber.
 - output limit: 205 (~1000mA)
 
 ![](run23/chart.png)
+
+## run24
+
+- supply: 5V
+- p: 16.0
+- i: 2.0
+- d: 6.4
+- period: 1000ms
+- output limit: 205 (~1000mA)
+
+![](run24/chart.png)
