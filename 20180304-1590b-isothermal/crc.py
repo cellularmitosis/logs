cfc05f13205460ab706fb29c84627fa72db448a2
Jason Pepas <jasonpepas@gmail.com>
2018-03-04 20:43:51 -0600

    arduino sketch

---
+++ 20180304-1590b-isothermal/crc.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+def crc16_xmodem(data):
+    # Returns the hex digits of the CRC16 CCITT (XModem) value.
+    # Thanks to https://stackoverflow.com/a/30357446
+    crc = 0
+    msb = crc >> 8
+    lsb = crc & 0xFF
+    for c in data:
+        x = ord(c) ^ msb
+        x ^= (x >> 4)
+        msb = (lsb ^ (x >> 3) ^ (x << 4)) & 0xFF
+        lsb = (x ^ (x << 5)) & 0xFF
+    crc = (msb << 8) + lsb
+    return hex(crc)[2:]
+
+assert crc16_xmodem("hello") == "c362"
+assert crc16_xmodem("123456789") == "31c3"
