62d79a76bfc2a1d2e8606e3679cdf33c16456dc1
Jason Pepas <jasonpepas@gmail.com>
2018-03-19 00:36:44 -0500

    sketch

--- 20180314-ambient/Arduino/fan_control/fan_control.ino
+++ 20180314-ambient/Arduino/fan_control/fan_control.ino
@@ -4,15 +4,16 @@
 
 // --- Configurable parameters ---
 
-// Average together this many samples from the Si7021
-uint8_t oversample = 1;
-
 // PID constants
-double kp = 1000.0;
-double ki = 10.0;
+double kp = 500.0;
+double ki = 30.0;
 double kd = 0.0;
-uint16_t loop_period = 1000; // in ms
 double setpoint_c = 29.0; // in celsius
+
+// how long it typically takes to read from the Si7012, in milliseconds;
+uint8_t temperature_read_delay = 202 + 2; // actually 201 or 202, but we add a small safety margin.
+uint16_t loop_period = temperature_read_delay * 2; // in ms
+
 
 // exhaust fan
 int8_t fanPin = 4;
@@ -61,12 +62,11 @@ void setup() {
   Serial.println("\nSTARTING");
   
   // Print the CSV header.
-  Serial.println("ambient_c,ambient_rh,duty_cycle,crc16");
+  Serial.println("ambient_c,ambient_rh,set_c,err_c,duty_cycle,crc16");
 
   start = millis();
   next_loop_start = millis() + loop_period;
 }
-
 
 void loop() {
   
@@ -80,41 +80,63 @@ void loop() {
   uint16_t on_time = loop_period * duty_cycle; // in milliseconds
   uint16_t off_time; loop_period - on_time; // in milliseconds
 
-  // turn the fan off
-  digitalWrite(fanPin, LOW);
+  double rh_accumulator = 0;
+  double c_accumulator = 0;
+  uint8_t sample_count = 0;
 
-  while(millis() < (next_loop_start - on_time)) {
-    delay(1);
-  }
+  if (duty_cycle < 0.5) {
 
-  // turn the fan on
-  digitalWrite(fanPin, HIGH);
+    // turn the fan off
+    digitalWrite(fanPin, LOW);
 
-  // we assume that the fan will be on for long enough for the following to take place:
+    // read the temperature.  this takes about 202ms.
+    rh_accumulator += ambient.getRH();
+    c_accumulator += ambient.readTemp();
+    sample_count += 1;
 
-  // sample the temperature
-  if (oversample > 1) { 
-    double rh_accumulator = 0;
-    double c_accumulator = 0;
-    for(uint8_t i=0; i < oversample; i++) {
-      rh_accumulator += ambient.getRH();
-      c_accumulator += ambient.readTemp();
+    // busy-wait while the fan should be off
+    while(millis() < (next_loop_start - on_time)) {
+      delay(1);
     }
-    humidity = rh_accumulator / oversample; 
-    temperature_c = c_accumulator / oversample; 
+
+    // turn the fan on
+    digitalWrite(fanPin, HIGH);
+  
   } else {
-      humidity = ambient.getRH();
-      temperature_c = ambient.readTemp();    
+
+    // turn the fan off
+    digitalWrite(fanPin, LOW);
+
+    // busy-wait while the fan should be off
+    while(millis() < (next_loop_start - on_time)) {
+      delay(1);
+    }
+
+    // turn the fan on
+    digitalWrite(fanPin, HIGH);
+
+    // read the temperature.  this takes about 202ms.
+    rh_accumulator += ambient.getRH();
+    c_accumulator += ambient.readTemp();
+    sample_count += 1;
   }
+  
+  humidity = rh_accumulator / sample_count; 
+  temperature_c = c_accumulator / sample_count; 
+  
   pid_input = temperature_c;
 
   // construct the logging message
-  char buf[32];
+  char buf[64];
   char *ptr = buf;
   char float_buf[16];
   dtostrf(temperature_c, 1, 3, float_buf);
   ptr += sprintf(ptr, "%s", float_buf);
   dtostrf(humidity, 1, 3, float_buf);
+  ptr += sprintf(ptr, ",%s", float_buf);
+  dtostrf(setpoint_c, 1, 3, float_buf);
+  ptr += sprintf(ptr, ",%s", float_buf);
+  dtostrf(temperature_c - setpoint_c, 1, 3, float_buf);
   ptr += sprintf(ptr, ",%s", float_buf);
   dtostrf(duty_cycle, 1, 3, float_buf);
   ptr += sprintf(ptr, ",%s", float_buf);
